508d713cc6211d7ff4c8426aa9182321f102e73e
elwazatikhadija <127736014+elwazatikhadija@users.noreply.github.com>
2023-07-21 16:52:48 +0100

    DM-22-border fix iphone

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -34,7 +34,7 @@ export default function SignUp() {
                     type="text"
                     autoComplete="firstName"
                     required
-                    className="w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6 "
+                    className="w-full px-2 rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 border-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6 "
                   />
                 </div>
               </div>
