a833894ae0500044a26e9fa3e190872b5f1003d3
elwazatikhadija <127736014+elwazatikhadija@users.noreply.github.com>
2023-08-08 12:34:53 +0100

    add Dashboard partner + historique action

--- src/Pages/Partner/Dashboard_Partner.js
+++ src/Pages/Partner/Dashboard_Partner.js
@@ -1,7 +1,72 @@
-import React from 'react'
-
-export default function Dashboard_Partner() {
+import React, { useState } from 'react';
+import SideBar from './SideBarPartner';
+import UserEvents from '../UserEvents';
+const Dashboard_Partner = () => {
+  const [activeMenuItem, setActiveMenuItem] = useState(window.location.hash.substring(1) || "History");
+  const handleMenuItemClick = (menuTitle) => {
+    setActiveMenuItem(menuTitle);
+  };
   return (
-    <div>Dashboard_Partner</div>
-  )
+    <div className="flex px-5  gap-x-4 relative">
+      <SideBar handleMenuItemClick={handleMenuItemClick} />
+      <div className="flex-1 -mt-8">
+       
+        {activeMenuItem === "Dashboard" && (
+          <div className=" ">
+           
+            <h1>Dashboard Content</h1>
+          </div>
+        )}
+        {activeMenuItem === "My Profil" && (
+          <div>
+           
+            <h1>My Profil Content</h1>
+          </div>
+        )}
+        {activeMenuItem === "Sponsoring" && (
+          <div>
+          
+            <h1>Sponsoring Content</h1>
+          </div>
+        )}
+          {activeMenuItem === "Events" && (
+          <div>
+          
+            <h1>My events Content</h1>
+          </div>
+        )}
+         {activeMenuItem === "Investors" && (
+          <div>
+          
+            <h1>My Investors Content</h1>
+          </div>
+        )}
+          {activeMenuItem === "Documents" && (
+          <div>
+          
+            <h1>My Documents Content</h1>
+          </div>
+        )}
+          {activeMenuItem === "Notifications" && (
+          <div>
+          
+            <h1>My Notifications Content</h1>
+          </div>
+        )}
+         {activeMenuItem === "History" && (
+          <UserEvents />
+        )}
+        {activeMenuItem === "Setting" && (
+          <div>
+          
+            <h1>My Setting Content</h1>
+          </div>
+        )}
+        
+
+     
+      </div>
+    </div>
+  );
 }
+export default Dashboard_Partner;
