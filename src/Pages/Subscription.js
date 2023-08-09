9f6f8cbb8a891532fcf6fa11405cc39fe01ff86d
ouardini14 <ouardini14@gmail.com>
2023-08-09 17:35:44 +0100

    DM-62 Adding Subscription Billing Section

--- src/Pages/Subscription.js
+++ src/Pages/Subscription.js
@@ -26,30 +26,30 @@ export default function Subscription() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center  md:space-y-8  px-10 ">
+    <div className="flex flex-col items-center justify-center  md:space-y-8  px-10 pt-10 ">
       <Toaster />
-      <h1 className="text-5xl font-bold mb-8">Unlock More Features</h1>
+      <h1 className="text-xl md:text-4xl xl:text-5xl font-bold mb-8 text-center">Unlock More Features</h1>
       {
         (isLoading || response.isLoading )  && 'Loading'
       }
    
-      <div className="grid gap-8 grid-cols-1 md:grid-cols-3">
+      <div className="flex gap-8 items-center justify-center flex-wrap xl:flex-nowrap ">
 
           
           {
           data && data.length > 0 && !response.isLoading &&
             data?.map((el,i)=>(
 
-              <div className={`bg-${el.name.toLowerCase()} rounded-lg p-8 shadow-xl  md:w-[330px]`}  key={i}>
+              <div className={`bg-${el.name.toLowerCase()} rounded-lg p-8 shadow-xl  md:w-[290px] xl:w-[330px]`}  key={i}>
                 <h4 className="text-3xl font-semibold mb-4">Pack {el.name}</h4>
                 <h2 className="text-xl font-semibold mb-4">
-                  <span className="font-bold">${el.price}</span><span className="text-gray-500 italic">/{el.duration/30} months</span>
+                  <span className="font-bold">${el.price}</span><span className="text-gray-500 italic">/{el.duration/30} month(s)</span>
                 </h2>
                 <button
-                  disabled={userInfo?.member?.subscriptionId === el._id || response?.isLoading}
+                  disabled={response?.isLoading}
                   onClick={() => { buySub(el._id) }}
-                className="px-4 py-2 bg-green-600 text-white rounded-md shadow-md disabled:bg-opacity-10 disabled:cursor-not-allowed" >
-                  {userInfo?.member?.subscriptionId === el._id ?   "Purchased" : "Buy Now"}
+                  className={`${userInfo?.member?.subscriptionId ? 'bg-yellow-400' : 'bg-green-600'}  px-4 py-2  text-white rounded-md shadow-md disabled:bg-opacity-10 disabled:cursor-not-allowed`}>
+                  {userInfo?.member?.subscriptionId  ?   "Renew" : "Buy Now"}
                 </button>
                 <ul className='list-disc text-sm p-8 text-gray-500 italic space-y-1'>
                   <li>You get {el.credits} credits per month</li>
