a15c2cbcef72eea9d0ed79aae0537acc8409d06e
ouardini14 <ouardini14@gmail.com>
2023-08-10 14:22:30 +0100

    Create_Project.js

--- src/Pages/Member/Create_Project.js
+++ src/Pages/Member/Create_Project.js
@@ -228,7 +228,7 @@ const Create_Project = () => {
                                             type="text"
                                             value={member.role}
                                             onChange={(e) => handleTeamMemberRoleChange(index, e.target.value)}
-                                            placeholder={`Role ${index + 1}`}
+                                            placeholder={`Role`}
                                             className="block flex-1 w-full px-2 rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 border-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
                                         />
                                         <button
