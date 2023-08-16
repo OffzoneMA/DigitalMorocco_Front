ec0158875698f61df937928d45d5ce4417babde4
elwazatikhadija <127736014+elwazatikhadija@users.noreply.github.com>
2023-08-16 17:26:27 +0100

    fixing components title

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -1,13 +1,12 @@
 import React from "react";
+import PageTitle from "../Components/PageTitle";
+import { CheckIcon} from '@heroicons/react/24/solid'
 const Pricing = () => {
     return (
-        <div className="flex flex-col items-center justify-center md:space-y-8 pt-16 md:pt-48 py-8 md:py-56 px-4 md:px-16">
-            <h1 className="text-sm md:text-sm font-normal mb-2 md:mb-3 text-gray-400 text-center">
-                PLAN & PRICING
-            </h1>
-            <h1 className="text-3xl md:text-5xl font-semibold mb-4 md:mb-8 text-bleu2 text-center">
-                Simple & Flexible Pricing built <br /> for Everyone
-            </h1>
+        <div className="  ">
+            <PageTitle subtitle={'PLAN & PRICING'}title={'Simple & flexible pricing built for everyone'}text={''}/>
+        <div className="flex flex-col items-center justify-center md:space-y-8   px-4 md:px-16">
+            
             <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 xl:gap-12 2xl:gap-16">
                 <div className=" w-full md:w-[382px] flex flex-col items-center mb-8 md:mb-0 ">
                     <div className="bg-white p-6 py-9 rounded-3xl border border-gray-300 flex flex-col items-center w-full">
@@ -22,11 +21,11 @@ const Pricing = () => {
                         </p>
                     </div>
                     <div className=" text-gray-400 py-8 px-3 ">
-                        <ul className="list-disc list-inside space-y-4 tracking-wider">
-                            <li>Access to Digital Morocco networking platform</li>
-                            <li>Attend select online events and webinars</li>
-                            <li>Limited access to curated industry resources</li>
-                            <li>Connect with potential partners and investors</li>
+                        <ul className="  space-y-4 tracking-wider">
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Access to Digital Morocco networking platform</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Attend select online events and webinars</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Limited access to curated industry resources</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Connect with potential partners and investors</li>
                         </ul>
                     </div>
                     <div className="text-center  text-gray-400 py-8">
@@ -48,12 +47,12 @@ const Pricing = () => {
                         </p>
                     </div>
                     <div className=" text-gray-400 py-8 px-3">
-                        <ul className="list-disc list-inside space-y-4 tracking-wider">
-                            <li>Full access to the Digital Morocco networking platform</li>
-                            <li>Unlimited access to all online events, conferences, and webinars</li>
-                            <li>Exclusive access to premium industry resources and reports</li>
-                            <li>Priority connections with investors and partners</li>
-                            <li>Featured profile placement and visibility</li>
+                        <ul className="  space-y-4 tracking-wider">
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Full access to the Digital Morocco networking platform</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Unlimited access to all online events, conferences, and webinars</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Exclusive access to premium industry resources and reports</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Priority connections with investors and partners</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Featured profile placement and visibility</li>
                         </ul>
                     </div>
                     <div className="text-center  text-white py-8">
@@ -75,12 +74,12 @@ const Pricing = () => {
                         </p>
                     </div>
                     <div className=" text-gray-400 py-8 px-3">
-                        <ul className="list-disc list-inside space-y-4 tracking-wider">
-                            <li>VIP access to the Digital Morocco</li>
-                            <li>Access to all online and offline events, conferences, and webinars</li>
-                            <li>Full access to premium industry resources, reports, and workshops</li>
-                            <li>Dedicated account manager for personalized connections and support</li>
-                            <li>Strategic promotion and branding opportunities</li>
+                        <ul className="  space-y-4 tracking-wider">
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>VIP access to the Digital Morocco</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Access to all online and offline events, conferences, and webinars</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Full access to premium industry resources, reports, and workshops</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Dedicated account manager for personalized connections and support</li>
+                            <li className="flex gap-5"><CheckIcon className="text-col1 h-5 w-8 "/>Strategic promotion and branding opportunities</li>
                         </ul>
                     </div>
                     <div className="text-center  text-gray-400 py-8">
@@ -90,6 +89,7 @@ const Pricing = () => {
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
