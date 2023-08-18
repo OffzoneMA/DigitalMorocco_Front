b9ef2c29e491f83a96349c9b71016d1ca85e2b00
ouassima <elyakoubi.ouassima@gmail.com>
2023-08-18 07:47:29 +0100

    DM-80 responsive landing page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,18 +8,18 @@ export default function Home() {
   return (
 
     <div>
-      <div className="flex items-center justify-center md:items-start md:justify-start px-5 sm:px-20   md:pt-48 md:pl-48   w-full h-screen bg-[#182049] bg-[url(/public/img/Bg.png),_url(/public/img/MorrocoMap.png)]  bg-no-repeat bg-center md:bg-right">
-        <div className="flex flex-col items-center justify-center  md:items-start md:justify-start   md:max-w-[514px] space-y-4  ">
-          <h2 className=" text-center md:text-left text-3xl md:text-6xl font-bold text-white ">Grow your<br />business through networking and digital solutions!</h2>
+      <div className="flex items-center justify-center md:items-start md:justify-start px-5 sm:px-20 md:pt-40 md:pl-48 w-full h-screen bg-[#182049] bg-[url(/public/img/Bg.png),_url(/public/img/MorrocoMap.png)]  bg-no-repeat bg-center md:bg-right">
+        <div className="flex flex-col items-center justify-center  md:items-start md:justify-start md:max-w-[638px] space-y-4 md:space-y-0 xl:py-10 lg:py-10 md:py-5 ">
+          <h2 className=" text-center md:text-left text-3xl md:text-6xl font-bold text-white ">Grow your business through networking and digital solutions!</h2>
           <p className=" text-center md:text-left py-3 tracking-wide font-normal  text-white md:text-opacity-50 ">Expand your professional network, gain access to exclusive resources, and connect with individuals and organizations committed to advancing the digital landscape in Morocco.</p>
           <button className=" px-3 py-2 rounded-full justify-center tracking-wide w-fit bg-blue-500 text-lg font-normal leading-6 text-white shadow-sm  hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">Get Started Now</button>
         </div>
 
       </div>
       <WhoAreWe />
-     <WhyJoinUs />
+      <WhyJoinUs />
       <OurGoals />
-      <OurActivities /> 
+      <OurActivities />
 
     </div>
 
--- src/Pages/LandingPage/WhyJoinUs.js
+++ src/Pages/LandingPage/WhyJoinUs.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function WhyJoinUs(){
     return(
-        <div className="w-[1440px] h-[720px] py-0 pl-[233px] bg-[#F2F4F7] grid grid-cols-2  ">
-            <div>
-            <div className="w-[552px] h-[720px] -mx-28 my-20 -px-8">
-                <h1 className="font-bold text-xs text-gray700 tracking-wider py-3 " >WHY JOIN US?</h1>
-                <h1 className="font-bold text-4xl text-gray700 leading-normal ">Discover a dynamic and innovative business networking platform</h1>
-                <p className="font-semibold text-xl text-gray700 leading-normal  tracking-wide py-6 ">Digital Morocco is dedicated to fostering meaningful connections and driving business success.</p>
+        <div className="flex flex-col sm:flex-col md:flex-row xl:flex-row lg:flex-row items-center justify-center md:items-start md:justify-start md:h-screen lg:h-screen xl:h-screen bg-[#F2F4F7]">
+           <div>
+            <div className="flex flex-col items-center justify-center md:items-start md:justify-start py-8 xl:pl-40 xl:pr-30 xl:py-20  md:px-10 md:py-10 lg:py-20 lg:pl-20 lg:pr-10 xl:mx-16 xl:space-y-2 sm:p-2">
+                <h1 className="font-bold text-xs text-gray700 tracking-wider py-3 md:py-2 lg:py-3 xl:py-3" >WHY JOIN US?</h1>
+                <h1 className="font-bold text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl text-gray700 leading-normal text-center md:text-left lg:text-left xl:text-left">Discover a dynamic and innovative business networking platform</h1>
+                <p className="font-semibold text-medium sm:text-lg md:text-lg lg:text-xl xl:text-xl text-gray700 leading-normal text-center tracking-wide md:text-left xl:text-left lg:text-left xl:py-6 lg:py-6 md:py-2  ">Digital Morocco is dedicated to fostering meaningful connections and driving business success.</p>
                 
-                <ul className="list-disc h-[231px] font-medium text-sm/[16px] text-gray700 py-5 tracking-wide leading-loose pl-[30px] pr-[30px]">
+                <ul className="list-disc font-medium text-sm text-gray700 xl:py-3 lg:py-3 md:py-0 tracking-wide leading-loose pl-5 text-left">
                     <li>You will increase your visibility among professionals who share your vision and values.</li>
                     <li>Being part of a business network is an effective way to expand your contacts.</li>
                     <li>Participate in regular events and networking opportunities with professionals.</li>
@@ -18,9 +18,9 @@ export default function WhyJoinUs(){
                 </ul>
                 
             </div>
-            </div>
-            <div className="">
-                <img src="/img/Frame.png" alt="" className='w-[535px] h-[720px]' />
+            </div> 
+            <div className="pb-4 sm:pb-0 md:pb-0 lg:pb-0 xl:pb-0">
+                <img src="/img/Frame.png" alt="" className='max-w-sm lg:max-w-lg xl:max-w-lg md:max-w-sm sm:max-w-sm xl:object-cover lg:object-cover md:object-cover sm:object-cover sm:h-[400px]  md:h-screen h-[300px] lg:h-screen xl:h-screen' />
             </div>
         </div>
 )
